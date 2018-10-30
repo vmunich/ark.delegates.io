a2ffd5a8587a55900c9eff0648a9e38c7f65f504
Brian Faust <hello@basecode.sh>
2018-10-30 19:48:45 +0200

    remove Analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,15 +35,6 @@
         <link rel="shortcut icon" href="{{ asset('favicon.png')}}">
         @include('layouts.partials.cookie-consent')
         @stack('styles')
-        {{-- Analytics --}}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126818980-1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-126818980-1');
-        </script>
     </head>
 
     <body class="font-sans">
