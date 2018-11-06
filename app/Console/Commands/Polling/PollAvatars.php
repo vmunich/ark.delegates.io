8640ad844cbbdb2220b374fa28aa191ed2095096
Brian Faust <hello@basecode.sh>
2018-11-06 08:12:12 +0200

    handle non-existent arkvatars via cronjob

--- app/Console/Commands/Polling/PollAvatars.php
+++ app/Console/Commands/Polling/PollAvatars.php
@@ -18,29 +18,47 @@ class PollAvatars extends Command
     protected $signature = 'ark:poll:avatars';
 
     /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        $this->client = GuzzleFactory::make([
+            'base_uri'        => 'https://www.arkvatar.com/arkvatar/',
+            'allow_redirects' => false,
+        ]);
+    }
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
-    public function handle(Client $client)
+    public function handle()
     {
-        $client = GuzzleFactory::make([
-            'base_uri'        => 'https://www.arkvatar.com/arkvatar/',
-            'allow_redirects' => false,
-        ]);
-
-        Delegate::all()->each(function ($delegate) use ($client) {
+        Delegate::all()->each(function ($delegate) {
             $logo = $delegate->extra_attributes->profile['logo'];
-            $logoExists = Storage::disk('public')->exists($logo);
 
-            if (! $logo || ! $logoExists) {
-                $this->line('Polling Delegate: <info>'.$delegate['username'].'</info>');
+            if (! $logo) {
+                return $this->pollAvatar($delegate);
+            }
 
-                $response = $client->get($delegate['address']);
-
-                $delegate->extra_attributes->set('profile.logo', $response->getHeader('Location')[0]);
-                $delegate->save();
+            if (! Storage::disk('public')->exists($logo)) {
+                return $this->pollAvatar($delegate);
             }
         });
+    }
+
+    /**
+     * Poll an arkvatar and store it.
+     * @param  Delegate $delegate
+     * @return void
+     */
+    private function pollAvatar (Delegate $delegate): void {
+        $this->line('Polling Delegate: <info>'.$delegate['username'].'</info>');
+
+        $response = $this->client->get($delegate['address']);
+
+        $delegate->extra_attributes->set('profile.logo', $response->getHeader('Location')[0]);
+        $delegate->save();
     }
 }
