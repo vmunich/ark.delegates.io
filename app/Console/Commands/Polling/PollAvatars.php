d2d5155a45ae843ae5018f123e3c0afc0df4ab84
faustbrian <faustbrian@users.noreply.github.com>
2018-11-06 08:12:32 +0200

    Apply fixes from StyleCI (#7)

--- app/Console/Commands/Polling/PollAvatars.php
+++ app/Console/Commands/Polling/PollAvatars.php
@@ -2,11 +2,11 @@
 
 namespace App\Console\Commands\Polling;
 
+use Storage;
 use App\Models\Delegate;
 use App\Services\Ark\Client;
 use Illuminate\Console\Command;
 use GrahamCampbell\GuzzleFactory\GuzzleFactory;
-use Storage;
 
 class PollAvatars extends Command
 {
