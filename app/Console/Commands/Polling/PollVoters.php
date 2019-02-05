0e3cd567549575788608caa8634d48ca38b70891
Brian Faust <hello@basecode.sh>
2019-02-05 08:16:49 +0200

    poll voters with a job

--- app/Console/Commands/Polling/PollVoters.php
+++ app/Console/Commands/Polling/PollVoters.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\Polling;
 
 use App\Models\Voter;
+use App\Jobs\PollVoters as Job;
 use App\Models\Delegate;
 use App\Services\Ark\Client;
 use App\Events\VoteWasShifted;
@@ -24,60 +25,8 @@ class PollVoters extends Command
      */
     public function handle(Client $client)
     {
-        $delegates = Delegate::all();
-
-        foreach ($delegates as $delegate) {
-            $this->line('Polling Voters: <info>'.$delegate['username'].'</info>');
-
-            // Current vote data...
-            $votesBefore = $delegate->voters()->sum('balance');
-            $activeVoters = $delegate->voters()->pluck('address');
-
-            // Store voters...
-            $votersList = collect($client->voters($delegate['public_key']));
-
-            // Update each voter...
-            foreach ($votersList as $voter) {
-                $this->line('Polling Voter: <info>'.$voter['address'].'</info>');
-
-                // Update...
-                $delegate->voters()->updateOrCreate([
-                    'address' => $voter['address'],
-                ], $voter);
-
-                // Used to be a voter...
-                $wasVoting = $activeVoters->contains($voter['address']);
-                $noLongerVoting = ! $votersList->pluck('address')->contains($voter['address']);
-
-                if ($wasVoting && $noLongerVoting) {
-                    Voter::whereAddress($voter['address'])->delete();
-                }
-            }
-
-            // Compare votes count...
-            $votesAfter = $delegate->voters()->sum('balance');
-
-            $this->triggerEvent($delegate, $votesBefore, $votesAfter);
-        }
-    }
-
-    /**
-     * Decide if we trigger an event.
-     *
-     * @param \App\Models\Delegate $delegate
-     * @param int                  $oldFigure
-     * @param int                  $newFigure
-     */
-    private function triggerEvent(Delegate $delegate, int $oldFigure, int $newFigure): void
-    {
-        if ($oldFigure <= 0 || $newFigure <= 0) {
-            return;
-        }
-
-        $percentChange = abs((1 - $oldFigure / $newFigure) * 100);
-
-        if ($percentChange >= 1) {
-            event(new VoteWasShifted($delegate));
+        foreach (Delegate::all() as $delegate) {
+            Job::dispatch($delegate);
         }
     }
 }
---
+++ app/Jobs/PollVoters.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Delegate;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class PollVoters implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var \App\Models\Delegate
+     */
+    public $delegate;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Delegate $delegate)
+    {
+        $this->delegate = $delegate;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Current vote data...
+        $votesBefore = $this->delegate->voters()->sum('balance');
+        $activeVoters = $this->delegate->voters()->pluck('address');
+
+        // Store voters...
+        $votersList = collect($client->voters($this->delegate['public_key']));
+
+        // Update each voter...
+        foreach ($votersList as $voter) {
+            // Update...
+            $this->delegate->voters()->updateOrCreate([
+                'address' => $voter['address'],
+            ], $voter);
+
+            // Used to be a voter...
+            $wasVoting = $activeVoters->contains($voter['address']);
+            $noLongerVoting = ! $votersList->pluck('address')->contains($voter['address']);
+
+            if ($wasVoting && $noLongerVoting) {
+                Voter::whereAddress($voter['address'])->delete();
+            }
+        }
+
+        // Compare votes count...
+        $votesAfter = $this->delegate->voters()->sum('balance');
+
+        $this->triggerEvent($this->delegate, $votesBefore, $votesAfter);
+    }
+
+    /**
+     * Decide if we trigger an event.
+     *
+     * @param \App\Models\Delegate $delegate
+     * @param int                  $oldFigure
+     * @param int                  $newFigure
+     */
+    private function triggerEvent(Delegate $delegate, int $oldFigure, int $newFigure): void
+    {
+        if ($oldFigure <= 0 || $newFigure <= 0) {
+            return;
+        }
+
+        $percentChange = abs((1 - $oldFigure / $newFigure) * 100);
+
+        if ($percentChange >= 1) {
+            event(new VoteWasShifted($delegate));
+        }
+    }
+}
